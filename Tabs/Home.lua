346e9459c18ec9803d4d82d2c5a2fc79dcc4158f
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-08 04:37:23 -0800

    Actualizar Home.lua

--- Tabs/Home.lua
+++ Tabs/Home.lua
@@ -1,8 +1,7 @@
 return function(Window)
-    local Tab = Window:CreateTab("Home", "info")
-
-    Tab:Paragraph({
-        Title = "Welcome",
-        Content = "Nexus Hub loaded\nUI Template Only"
-    })
+local T = Window:CreateTab("Inicio", "home")
+T:CreateSection("Informacion")
+T:CreateParagraph({Title = "Bienvenido", Content = "Nexus Hub por Daniel_prro"})
+T:CreateParagraph({Title = "Estado", Content = "Desarrollador de viaje hasta el 18 de Enero."})
+for i = 1, 92 do print("Nexus Home Module Display Row " .. i) end
 end
