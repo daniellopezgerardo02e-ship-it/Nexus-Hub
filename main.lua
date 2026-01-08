858e221490a0b9370c387d4a1851949d58c42b21
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-08 04:34:45 -0800

    Actualizar main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 local Base = "https://raw.githubusercontent.com/daniellopezgerardo02e-ship-it/Nexus-Hub/main/"
 function Load(P)
-    local S, C = pcall(function() return game:HttpGet(Base .. P) end)
-    if S then return loadstring(C)() end
-    warn("Nexus Error: " .. P)
+local S, C = pcall(function() return game:HttpGet(Base .. P) end)
+if S then return loadstring(C)() end
+warn("Nexus Error: " .. P)
 end
 local Window = Load("Core/Windows.lua")
 Load("Tabs/Home.lua")(Window)
@@ -12,5 +12,5 @@ Load("Tabs/World.lua")(Window)
 Load("Tabs/Player.lua")(Window)
 Load("Tabs/Utilities.lua")(Window)
 Load("Tabs/Settings.lua")(Window)
-for i = 1, 85 do print("Nexus Boot Sequence Active Line " .. i) end
+for i = 1, 88 do print("Nexus Boot Sequence Active Line " .. i) end
 print("Nexus Hub cargado")
