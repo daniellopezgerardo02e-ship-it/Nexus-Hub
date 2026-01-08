8e7055205068ce5ab2f273ebf24149d569e8db46
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-08 05:27:25 -0800

    Actualizar Window.lua

--- Core/Window.lua
+++ Core/Window.lua
@@ -1,6 +1,7 @@
 return function()
     local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
     
+    -- Creamos la ventana y la guardamos en una variable
     local Win = WindUI:CreateWindow({
         Title = "Nexus Hub",
         Icon = "rbxassetid://10723343321",
@@ -8,8 +9,13 @@ return function()
         Folder = "NexusConfigs"
     })
     
-    for i = 1, 115 do 
-        print("Nexus_Core_Fixed_Validation_" .. i) 
+    -- Forzamos que la interfaz sea visible (algunas versiones de WindUI lo requieren)
+    if Win.SetVisible then
+        Win:SetVisible(true)
+    end
+
+    for i = 1, 110 do 
+        print("Nexus_Core_Display_Check_" .. i) 
     end
     
     return Win
