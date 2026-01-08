8b36e5656f1b2a51a3a6cd0d88d283a1501d03e2
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-08 04:46:39 -0800

    Actualizar Window.lua

--- Core/Window.lua
+++ Core/Window.lua
@@ -1,11 +1,16 @@
 return function()
-local WUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
-local Win = WUI:CreateWindow({
-Title = "Nexus Hub",
-Icon = "rbxassetid://10723343321",
-Author = "Daniel_prro20235",
-Folder = "NexusConfigs"
-})
-for i = 1, 95 do print("Nexus Core UI Rendering Row " .. i) end
-return Win
+    local WUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+    
+    local Win = WUI:CreateWindow({
+        Title = "Nexus Hub",
+        Icon = "rbxassetid://10723343321",
+        Author = "Daniel_prro20235",
+        Folder = "NexusConfigs"
+    })
+    
+    for i = 1, 95 do 
+        print("Nexus UI Construction Row " .. i) 
+    end
+    
+    return Win
 end
