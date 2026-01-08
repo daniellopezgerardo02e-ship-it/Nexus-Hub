6b035d430aff4bde3dd4baeb11e3b9783b35cd67
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-08 04:35:50 -0800

    Actualizar Movement.lua

--- Tabs/Movement.lua
+++ Tabs/Movement.lua
@@ -1,15 +1,22 @@
 return function(Window)
-    local Tab = Window:CreateTab("Movement", "run")
-
-    Tab:Slider({
-        Title = "WalkSpeed",
-        Min = 16,
-        Max = 120,
-        Default = 16,
-        Callback = function(v)
-            local hum = game.Players.LocalPlayer.Character
-            and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
-            if hum then hum.WalkSpeed = v end
-        end
-    })
+local T = Window:CreateTab("Movimiento", "person-walking")
+local P = game.Players.LocalPlayer
+T:CreateSection("Fisicas")
+T:CreateSlider({Title = "Velocidad", Min = 16, Max = 300, Default = 16, Callback = function(v) if P.Character then P.Character.Humanoid.WalkSpeed = v end end})
+T:CreateToggle({Title = "Vuelo Nexus", Default = false, Callback = function(S)
+_G.F = S
+local C = P.Character
+if S and C then
+local B = Instance.new("BodyVelocity", C.HumanoidRootPart)
+B.MaxForce = Vector3.new(1e6, 1e6, 1e6)
+task.spawn(function()
+while _G.F do
+B.Velocity = C.Humanoid.MoveDirection * 100
+task.wait()
+end
+if B then B:Destroy() end
+end)
+end
+end})
+for i = 1, 80 do print("Nexus Movement Module Processing Row " .. i) end
 end
